78c70fba52d6f0ec5b95ae023d24811632b32026
Valtteri Lahti <valtteri.lahti@atostek.com>
2025-07-17 14:05:34 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+A quick demo of using Kotlin plugins, instead of injecting code straight from the MainActivity. https://docs.flutter.dev/packages-and-plugins/developing-packages
+Probably contains a lot of junk and unnecessary files, but hopefully conveys the idea somewhat.
+
 To run: `flutter run -d android` or similar
--- ble_background_app/lib/main.dart
+++ ble_background_app/lib/main.dart
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('BLE Background Hello')),
+        appBar: AppBar(title: Text('BLE Scan')),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
